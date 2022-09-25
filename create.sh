c7d729ebe87a3dd6fa86e4fd361a5f316f185baa
anilt01 <tanil010398@gmail.com>
2022-09-24 20:34:34 -0400

    automation of creating ec2 instances

--- create.sh
+++ create.sh
@@ -16,7 +16,7 @@ create_ec2() {
 }
 
 ##Main program
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=centos-8-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 if [ -z "${AMI_ID}" ]; then
   echo "AMI_ID not found"
   exit 1

b36d36d13ea8a0fb1f68ebd80bd935e971b79980
anilt01 <tanil010398@gmail.com>
2022-10-17 18:10:01 -0400

    updating names in place of instance id's in prometheus server

--- create-with-env.sh
+++ create-with-env.sh
@@ -9,7 +9,7 @@ SG_NAME="allow-all"
 if [ "$1" == "dev" -o "$1" == "prod" ]; then
   ENV=$1
 else
-  echo "Environment name is needed"
+  echo "Environment name dev or prod is needed"
   exit 1
 fi
 
@@ -18,7 +18,7 @@ create_ec2() {
    PRIVATE_IP=$(aws ec2 run-instances \
        --image-id ${AMI_ID} \
        --instance-type t3.micro \
-       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}},{Key=Monitor,Value=yes},{Key=Environment,Value=${ENV}},{Key=Component,Value=${component}}]"  \
+       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}},{Key=Monitor,Value=yes},{Key=Environment,Value=${ENV}},{Key=COMPONENT,Value=${component}}]"  \
        --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
        --security-group-ids ${SGID} \
        | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
